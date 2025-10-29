5b89733c071d594448589e9322e64e363d4d14d2
Deezinn <developer.andrelz@gmail.com>
2025-10-28 22:13:30 -0300

    feat: transform method to private

--- src/etl/transform/transform_all.py
+++ src/etl/transform/transform_all.py
@@ -6,16 +6,16 @@ class TransformAll(TransformAllInterface):
     def __init__(self):
         self._raw_dataframe = None
         
-    
     def process(self, raw: str):
         """Recebe o conjunto de dados em json e transforma em dataframe"""
     
         raw_data_normalized = pd.json_normalize(raw)
         self._raw_dataframe = pd.DataFrame(raw_data_normalized)
-        self.transform_orquestrador()
+        
+        self._transform_orquestrador()
         return self._raw_dataframe
     
-    def transform_orquestrador(self):
+    def _transform_orquestrador(self):
         self._translate_columns()
         self._convert_types()
         self._sanitize_columns()
--- src/interfaces/transform_interfaces/transform_all_interface.py
+++ src/interfaces/transform_interfaces/transform_all_interface.py
@@ -7,6 +7,11 @@ class TransformAllInterface(ABC):
         pass
 
     @abstractmethod
+    def _translate_columns(self):
+        """Tradução de colunas do dataset para nomes amigáveis em português"""
+        pass
+    
+    @abstractmethod
     def _convert_types(self):
         """Converte colunas para tipos ideais (int16, int32, int64, float32, datetime)."""
         pass
@@ -14,9 +19,4 @@ class TransformAllInterface(ABC):
     @abstractmethod
     def _sanitize_columns(self):
         """Trata valores ausentes e inconsistências nas colunas."""
-        pass
-
-    @abstractmethod
-    def _translate_columns(self):
-        """Tradução de colunas do dataset para nomes amigáveis em português"""
         pass
