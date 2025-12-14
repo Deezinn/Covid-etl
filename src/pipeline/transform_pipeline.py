c34351fb1eec43c02fb61a58a26cb7fd6abe68d0
Deezinn <developer.andrelz@gmail.com>
2025-12-14 19:52:21 -0300

    feat: create and apply interface from pipeline transform

--- src/pipeline/transform_pipeline.py
+++ src/pipeline/transform_pipeline.py
@@ -1,9 +1,9 @@
 import pandas as pd
-from domain.interfaces import TransformInterface
+from domain.interfaces import TransformPipelineInterface
 
 from .transformers import AllCases, States, Continents, Countries
 
-class TransformPipeline():
+class TransformPipeline(TransformPipelineInterface):
     def __init__(self):
         self.__registry = {
             'all_cases' : AllCases(),
@@ -18,9 +18,8 @@ class TransformPipeline():
          
         for key, data in datasets.items():
             raw_data[key] = data
-            
-            if self.__registry[key] is not None and self.__registry:
+            if self.__registry[key] is not None:
                 process_data[key] = self.__registry[key].transform(data=data)
-                
+        
         return raw_data, process_data
     
