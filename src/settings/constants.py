7854d31198773b053960c12242dda22c497df3c1
Deezinn <developer.andrelz@gmail.com>
2025-12-18 22:23:13 -0300

    feat: remove states

--- src/settings/constants.py
+++ src/settings/constants.py
@@ -2,7 +2,6 @@ BASEURL = 'https://disease.sh/v3/covid-19'
 
 APISURL = {
     'all_cases': f'{BASEURL}/all',
-    'states': f'{BASEURL}/states',
     'continents': f'{BASEURL}/continents',
     'countries': f'{BASEURL}/countries'
 }
